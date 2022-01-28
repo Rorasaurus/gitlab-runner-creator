5494841f234154b47cdfec53a150dd0d815434a0
Rory Swann <rory@shirenet.io>
2022-01-28 21:31:34 +0000

    Better README

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Gitlab Runner Creator
 
-This project contains a BASH script to allow facilitate the creation of a Docker Gitlab runner.
+This project contains a BASH script to facilitate the creation of a Docker Gitlab runner.
+
+## Usage
+
+The script can run from anywhere, but I'd suggest copying it to the home directory of a dedicated \n
+user. This user will require permission to execute the 'docker' command.
+
+It will create a series of directories to be used as Docker volumes. By default it will create:
+* /home/user/docker_volumes/                                ### Directory to store volumes 
+* /home/user/docker_volumes/[runner-name]/gitlab-runner     ### Volume stores Gitlab runner config directory
+* /home/user/docker_volumes/[runner-name]/.config           ### Generic store for standard Linux .config
+* /home/user/docker_volumes/[runner-name]/.docker           ### Store for Docker config
+* /home/user/docker_volumes/[runner-name]/.ssh              ### Store for ssh keys required by runner
+* /home/user/docker_volumes/[runner-name]/.secrets          ### Store for secrets required by runner
+
+I'd advice against storing your secrets and ssh keys this way, but it's there if you want it. Consider \n
+using something like Hashicorp Vault instead.
+
+Execute the script in the folling way:
+```
+./create_runner.sh -s [gitlab-server] -n [runner-name] -d [runner-description] -t [runner-token] -i [default-image] -l [runner-tags]
+```
+* -s [gitlab-server]        ### The address of your Gitlab server.
+* -n [runner-name]          ### The name of your Gitlab runner.
+* -d [runner-description]   ### Description of your runner.
+* -t [runner-token]         ### The token to register your runner.
+* -i [default-image]        ### The default image for your runner. Defaults to alpine:latest if not defined.
+* -l [runner-tags]          ### The tags to apply to your runner. Provide a comma seperated list if required.
+
+## Trouble shooting
+
+Don't forget to make sure script executable by running:
+```
+chmod +x create_runner.sh
+```
--- create_runner.sh
+++ create_runner.sh
@@ -9,20 +9,24 @@
 
 usage () {
     echo -e "\nUsage:\n"
-    echo -e "  $0 -n [runner-name] -d [runner-description] -t [runner-token] -i [default-image] -l [runner-tags]\n"
-    echo -e "  - runner-name must not contain spaces and should be lower case."
+    echo -e "  $0 -s [gitlab-server] -n [runner-name] -d [runner-description] -t [runner-token] -i [default-image] -l [runner-tags]\n"
+    echo -e "  - Required. gitlab-server is the same server you found your token."
+    echo -e "  - Required. runner-name must not contain spaces and should be lower case."
+    echo -e "  - Required. runner-description can contain spaces."
+    echo -e "  - Required. runner-token is found on yout Gitlab server."
     echo -e "  - default-image is not required. Defaults to alpine:latest if not set."
     echo -e "  - runner-tags should contain a comma seperated list. They are not required.\n"
 }
 
 # Get arguments
-while getopts :n:d:t:i:l: options; do
+while getopts :n:d:t:i:l:s: options; do
         case $options in
                 n) RUNNER_NAME=$OPTARG;;
                 d) RUNNER_DESC=$OPTARG;;
                 t) RUNNER_TOKEN=$OPTARG;;
                 i) RUNNER_IMAGE=$OPTARG;;
                 l) RUNNER_TAGS=$OPTARG;;
+                s) GITLAB_SERVER=$OPTARG;;
 
                 ?) usage && exit 1
         esac
@@ -30,37 +34,28 @@ done
 
 shift "$(( OPTIND - 1 ))"
 
-if [ -z "$RUNNER_NAME" ] || [ -z "$RUNNER_DESC" ] || [ -z $RUNNER_TOKEN ]; then
+if [ -z "${RUNNER_NAME}" ] || [ -z "${RUNNER_DESC}" ] || [ -z ${RUNNER_TOKEN} ] || [ -z ${GITLAB_SERVER} ]; then
         echo 'Missing required argument' >&2
         usage
         exit 1
 fi
 
-if [ -z "$RUNNER_IMAGE" ]; then
+if [ -z "${RUNNER_IMAGE}" ]; then
     echo -e "Default runner image not defined. Using 'alpine:latest'"
     RUNNER_IMAGE="alpine:latest"
 fi
 
 # Variables
-RUNNER_USER="docker-runner"
-GITLAB_SERVER="gitlab.shirenet"
-VOLUMES_DIR="/home/${RUNNER_USER}/docker_volumes"
+VOLUMES_DIR="${HOME}/docker_volumes"
 VOLUMES=(".config" "gitlab-runner" ".secrets" ".ssh" ".docker")
-
-# Check the current user is correct
-if [ $(whoami) == ${RUNNER_USER} ]; then
-    echo "Current user is ${RUNNER_USER}"
-else
-    echo "Terminating. Current user is $(whoami)"
-fi
 
 # Print the details
 echo -e "\nBuilding with the following variables:"
-echo "Runner name:          $RUNNER_NAME"
-echo "Runner description:   $RUNNER_DESC"
-echo "Runner image:         $RUNNER_IMAGE"
-echo "Runner tags:          $RUNNER_TAGS"
-echo -e "Runner token:         $RUNNER_TOKEN\n"
+echo "Runner name:          ${RUNNER_NAME}"
+echo "Runner description:   ${RUNNER_DESC}"
+echo "Runner image:         ${RUNNER_IMAGE}"
+echo "Runner tags:          ${RUNNER_TAGS}"
+echo -e "Runner token:         ${RUNNER_TOKEN}\n"
 
 # Check the user is sure
 while true
@@ -79,7 +74,7 @@ for i in ${VOLUMES[@]}; do
 done
 
 # Start and configure the Docker container containing the gitlab-runner process
-sudo docker run -d \
+docker run -d \
     --name ${RUNNER_NAME} --restart always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v ${HOME}/docker_volumes/${RUNNER_NAME}/gitlab-runner:/etc/gitlab-runner \
@@ -90,9 +85,9 @@ sudo docker run -d \
     gitlab/gitlab-runner:latest
 
 # Check the container is running. If so, register it with the Gitlab server
-if sudo docker ps | grep $RUNNER_NAME; then
+if docker ps | grep ${RUNNER_NAME}; then
     echo -e "\nRunner deployed successfully. Registering with ${GITLAB_SERVER}"
-    sudo docker exec ${RUNNER_NAME} gitlab-runner register --non-interactive --url "${GITLAB_SERVER}" --registration-token "${RUNNER_TOKEN}" --description "${RUNNER_DESC}" --executor "docker" --docker-image "${RUNNER_IMAGE}" --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" --tag-list "${RUNNER_TAGS}"
+    docker exec ${RUNNER_NAME} gitlab-runner register --non-interactive --url "${GITLAB_SERVER}" --registration-token "${RUNNER_TOKEN}" --description "${RUNNER_DESC}" --executor "docker" --docker-image "${RUNNER_IMAGE}" --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" --tag-list "${RUNNER_TAGS}"
 else
     echo -e "\nSomething went wrong. Please perform manual checks."
 fi
